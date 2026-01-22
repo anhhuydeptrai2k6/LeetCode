181997dcdb122368eb4145c5255fe8fc82d0c8b7
anhhuydeptrai2k6 <masterhades216@gmail.com>
2026-01-22 19:55:58 +0700

    LeetCode 22.01.2026

---
+++ 151.ts
@@ -0,0 +1,11 @@
+function reverseWords(s: string): string {
+    let cacTu : string[] = s.split(" ");
+    let tu : string[] = [];
+    for (let i = 0; i < cacTu.length; i++) {
+        if (cacTu[i] !== ""){
+            tu.push(cacTu[i]);
+        }
+    }
+    tu.reverse();
+    return tu.join(" ");
+};
