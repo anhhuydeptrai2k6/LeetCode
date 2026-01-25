07aff02e2339925c2039525512c04b602883c56d
anhhuydeptrai2k6 <masterhades216@gmail.com>
2026-01-25 20:45:44 +0700

    LeetCode 25.01.2026

---
+++ 443.ts
@@ -0,0 +1,20 @@
+function compress(chars: string[]): number {
+    let write : number = 0;
+    let dem : number = 1;
+
+    for (let i : number = 0; i < chars.length; i++) {
+        if (i + 1 < chars.length && chars[i] === chars[i + 1]) {
+            dem++;
+        } else {
+            chars[write++] = chars[i];
+            if (dem > 1) {
+                for (let c of dem.toString()) {
+                    chars[write++] = c;
+                }
+            }
+            dem = 1;
+        }
+    }
+
+    return write;
+}
