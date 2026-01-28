5fe0f0e13fdffdcdb951b119f27db068fc87c464
anhhuydeptrai2k6 <masterhades216@gmail.com>
2026-01-28 20:05:06 +0700

    LeetCode 28.01.2026

---
+++ 1679.ts
@@ -0,0 +1,21 @@
+function maxOperations(nums: number[], k: number): number {
+    let soLan : number = 0;
+
+    nums.sort((a, b) => a - b);
+    let left : number = 0;
+    let right: number = nums.length - 1;
+    while (left < right) {
+        const sum : number = (nums[left] + nums[right]);
+        if (sum === k){
+            soLan ++;
+            left ++;
+            right --;
+        } else if (sum < k){
+            left ++;
+        } else {
+            right --;
+        }
+    }
+
+    return soLan;
+}
