ba20a41a721fb43bbc65015d4d6abb13c2107b84
anhhuydeptrai2k6 <masterhades216@gmail.com>
2026-01-29 19:48:45 +0700

    LeetCode 29.01.2026

---
+++ 11.ts
@@ -0,0 +1,16 @@
+function maxArea(height: number[]): number {
+    let max : number = 0;
+
+    let left : number = 0;
+    let right: number = height.length - 1;
+    while (left < right) {
+        const s : number = Math.min(height[left], height[right]) * (right - left);
+        max = Math.max(s, max);
+        if (height[left] < height[right]){
+            left ++;
+        }
+        else right --;
+    }
+
+    return max;
+};
